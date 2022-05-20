72d251117e7f9de006a28fb19fd6200fa958dda3
JalonUniversal <453741411@qq.com>
2022-05-20 09:18:53 +0800

    fix: some  changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Jalon
+          Build With GithuB Action WorkFlows
         </p>
         <a
           className="App-link"
